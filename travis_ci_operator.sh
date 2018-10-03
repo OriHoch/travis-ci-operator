33cc79547746c5ee5631866a9f8abb2692adbdc0
Ori Hoch <ori@uumpa.com>
2018-10-03 12:22:36 +0300

    travis

--- travis_ci_operator.sh
+++ travis_ci_operator.sh
@@ -15,23 +15,23 @@ elif [ "${1}" == "docker-login" ]; then
     exit 0
 
 elif [ "${1}" == "github-update" ]; then
-    DEPLOY_KEY_NAME="${1}"
-    GIT_BRANCH="${2}"
-    UPDATE_SCRIPT="${3}"
-    COMMIT_MSG="${4}"
+    DEPLOY_KEY_NAME="${2}"
+    GIT_BRANCH="${3}"
+    UPDATE_SCRIPT="${4}"
+    COMMIT_MSG="${5}"
     [ -z "${DEPLOY_KEY_NAME}" ] || [ -z "${GIT_BRANCH}" ] || [ -z "${UPDATE_SCRIPT}" ] || [ -z "${COMMIT_MSG}" ] \
         && echo missing required arguments && exit 1
     [ "${DEPLOY_KEY_NAME}" == "self" ] && [ "${COMMIT_MSG}" == "${TRAVIS_COMMIT_MESSAGE}" ] && [ "${GIT_BRANCH}" == "${TRAVIS_BRANCH}" ] \
         && echo skipping update of self with same commit msg and branch && exit 0
     GITHUB_REPO_SLUG="${TRAVIS_REPO_SLUG}"
     [ -z "${GITHUB_REPO_SLUG}" ] && echo missing GITHUB_REPO_SLUG && exit 1
-    GITHUB_DEPLOY_KEY_FILE="travis_ci_operator_self_github_deploy_key.id_rsa"
+    GITHUB_DEPLOY_KEY_FILE="travis_ci_operator_${DEPLOY_KEY_NAME}_github_deploy_key.id_rsa"
     cp -f "${GITHUB_DEPLOY_KEY_FILE}" ~/.ssh/id_rsa && chmod 400 ~/.ssh/id_rsa
-    [ "$?" != "0" ] && echo echo failed to setup deploy key for pushing to GitHub && return 1
+    [ "$?" != "0" ] && echo echo failed to setup deploy key for pushing to GitHub && exit 1
     GIT_REPO="git@github.com:${GITHUB_REPO_SLUG}.git"
     TEMPDIR=`mktemp -d`
-    echo Cloning git repo ${GIT_REPO}
-    ! git clone --branch ${GIT_BRANCH} ${GIT_REPO} ${TEMPDIR} && echo failed to clone repo && return 1
+    echo Cloning git repo ${GIT_REPO} branch ${GIT_BRANCH}
+    ! git clone --branch ${GIT_BRANCH} ${GIT_REPO} ${TEMPDIR} && echo failed to clone repo && exit 1
     pushd $TEMPDIR
     eval "${UPDATE_SCRIPT}"
     echo Committing and pushing to GitHub repo ${GIT_REPO} branch ${GIT_BRANCH}
