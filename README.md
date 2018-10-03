92966b1fe4718f222d8b0a61f62885a4727ae247
Ori Hoch <ori@uumpa.com>
2018-10-03 13:48:01 +0300

    github-yaml-update

--- README.md
+++ README.md
@@ -92,3 +92,18 @@ The last argument is the repo slug of the other repo to update:
 script:
 - travis_ci_operator.sh github-update github-yaml-updater master "echo foo > bar; git add bar" "testing travis-ci-operator" OriHoch/github-yaml-updater
 ```
+
+### Updating yaml files in GitHub repos
+
+The following command will update yaml file `test.yaml` in self repo's master branch with the given commit message
+
+```
+- travis_ci_operator.sh github-yaml-update self master test.yaml '{"foo":"bar"}' "testing github yaml update"
+```
+
+The following command will update yaml file `test.yaml` in github-yaml-updater repo
+
+```
+script:
+- travis_ci_operator.sh github-update github-yaml-updater master "echo foo > bar; git add bar" "testing travis-ci-operator" OriHoch/github-yaml-updater
+```
--- travis_ci_operator.sh
+++ travis_ci_operator.sh
@@ -4,6 +4,8 @@ if [ "${1}" == "init" ]; then
     chmod +x $HOME/bin/travis_ci_operator.sh
     curl -L https://raw.githubusercontent.com/OriHoch/travis-ci-operator/master/read_yaml.py > $HOME/bin/read_yaml.py
     chmod +x $HOME/bin/read_yaml.py
+    curl -L https://raw.githubusercontent.com/OriHoch/travis-ci-operator/master/update_yaml.py > $HOME/bin/update_yaml.py
+    chmod +x $HOME/bin/update_yaml.py
     ! $(eval echo `read_yaml.py .travis-ci-operator.yaml selfDeployKeyDecryptCmd`) \
         && echo Failed to get self deploy key && exit 1
     echo Successfully initialized travis-ci-operator
@@ -46,6 +48,48 @@ elif [ "${1}" == "github-update" ]; then
     ! git clone --branch ${GIT_BRANCH} ${GIT_REPO} ${TEMPDIR} && echo failed to clone repo && exit 1
     pushd $TEMPDIR
     eval "${UPDATE_SCRIPT}"
+    echo Committing and pushing to GitHub repo ${GIT_REPO} branch ${GIT_BRANCH}
+    git commit -m "${COMMIT_MSG}" && ! git push ${GIT_REPO} ${GIT_BRANCH} \
+        && echo failed to push change to GitHub && exit 1
+    popd
+    rm -rf $TEMPDIR
+    echo GitHub update completed successfully
+    exit 0
+
+elif [ "${1}" == "github-yaml-update" ]; then
+    DEPLOY_KEY_NAME="${2}"
+    GIT_BRANCH="${3}"
+    YAML_FILE="${4}"
+    UPDATE_VALUES="${5}"
+    COMMIT_MSG="${6}"
+    if [ "${DEPLOY_KEY_NAME}" == "self" ]; then
+        GITHUB_REPO_SLUG="${TRAVIS_REPO_SLUG}"
+    else
+        GITHUB_REPO_SLUG="${7}"
+    fi
+    [ -z "${DEPLOY_KEY_NAME}" ] || [ -z "${GIT_BRANCH}" ] || [ -z "${UPDATE_SCRIPT}" ] || [ -z "${COMMIT_MSG}" ] \
+        && echo missing required arguments && exit 1
+    [ "${DEPLOY_KEY_NAME}" == "self" ] && [ "${COMMIT_MSG}" == "${TRAVIS_COMMIT_MESSAGE}" ] && [ "${GIT_BRANCH}" == "${TRAVIS_BRANCH}" ] \
+        && echo skipping update of self with same commit msg and branch && exit 0
+    [ -z "${GITHUB_REPO_SLUG}" ] && echo missing GITHUB_REPO_SLUG && exit 1
+    if [ "${DEPLOY_KEY_NAME}" != "self" ]; then
+        ! $(eval echo `read_yaml.py .travis-ci-operator.yaml ${DEPLOY_KEY_NAME}DeployKeyDecryptCmd`) \
+            && echo Failed to get deploy key && exit 1
+    fi
+    GITHUB_DEPLOY_KEY_FILE=".travis_ci_operator_${DEPLOY_KEY_NAME}_github_deploy_key.id_rsa"
+    if [ -e "${GITHUB_DEPLOY_KEY_FILE}" ]; then
+        cp -f "${GITHUB_DEPLOY_KEY_FILE}" ~/.ssh/id_rsa && chmod 400 ~/.ssh/id_rsa
+        [ "$?" != "0" ] && echo failed to setup deploy key for pushing to GitHub && exit 1
+    else
+        echo WARNING: deploy key file not found
+    fi
+    GIT_REPO="git@github.com:${GITHUB_REPO_SLUG}.git"
+    TEMPDIR=`mktemp -d`
+    echo Cloning git repo ${GIT_REPO} branch ${GIT_BRANCH}
+    ! git clone --branch ${GIT_BRANCH} ${GIT_REPO} ${TEMPDIR} && echo failed to clone repo && exit 1
+    pushd $TEMPDIR
+    ! update_yaml.py "${UPDATE_VALUES}" "${YAML_FILE}" \
+        && echo failed to update yaml file && exit 1
     echo Committing and pushing to GitHub repo ${GIT_REPO} branch ${GIT_BRANCH}
     git commit -m "${COMMIT_MSG}" && ! git push ${GIT_REPO} ${GIT_BRANCH} \
         && echo failed to push change to GitHub && exit 1
